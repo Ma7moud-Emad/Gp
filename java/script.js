1071da771b7995425d38b9dbae05cb4ab37db405
Mahmoud Alfaramawy <101274098+Ma7moud-Emad@users.noreply.github.com>
2023-04-24 16:48:42 +0200

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,26 +1,26 @@
-// start first page
-let startBotton = document.querySelector(".start");
-startBotton.onclick = function () {
-  document.querySelector(".start a").click();
-};
+// // start first page
+// let startBotton = document.querySelector(".start");
+// startBotton.onclick = function () {
+//   document.querySelector(".start a").click();
+// };
 
-let aboutBotton = document.querySelector(".about");
-aboutBotton.onclick = function () {
-  document.querySelector(".about a").click();
-};
+// let aboutBotton = document.querySelector(".about");
+// aboutBotton.onclick = function () {
+//   document.querySelector(".about a").click();
+// };
 
-let endBotton = document.querySelector(".end");
-endBotton.onclick = function () {
-  document.querySelector(".end a").click();
-};
-// end first page
-// start model page
-let sequenceInput = document.getElementById("sequence");
-sequenceInput.oninput = function () {
-  if (sequenceInput.value.length > 9) {
-    document.querySelector(".model p").style.opacity = "0";
-  } else if (sequenceInput.value.length <= 9) {
-    document.querySelector(".model p").style.opacity = "1";
-  }
-};
-// end model page
+// let endBotton = document.querySelector(".end");
+// endBotton.onclick = function () {
+//   document.querySelector(".end a").click();
+// };
+// // end first page
+// // start model page
+// let sequenceInput = document.getElementById("sequence");
+// sequenceInput.oninput = function () {
+//   if (sequenceInput.value.length > 9) {
+//     document.querySelector(".model p").style.opacity = "0";
+//   } else if (sequenceInput.value.length <= 9) {
+//     document.querySelector(".model p").style.opacity = "1";
+//   }
+// };
+// // end model page
